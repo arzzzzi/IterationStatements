c3cdd2d0eef9d21c3d24e21002010ae316ec7ed4
arzzzzi <kantyshevmagomed@gmail.com>
2023-03-01 12:58:18 +0300

    'loops'

--- Program.cs
+++ Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace IterationStatements
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            //FOR LOOP
+            //var name = "Maga Agam";
+
+            //for (var i = 0; i < name.Length; i++) 
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //FOREACH LOOP
+            //var numbers = new int[4] { 1, 2, 3, 4 };
+
+            //foreach (var number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //WHILE LOOP
+            while (true) 
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if(!String.IsNullOrWhiteSpace(input)) 
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+                break; 
+            }
         }
     }
 }
+ 
